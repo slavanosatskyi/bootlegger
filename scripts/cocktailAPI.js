641ced9cc5cb271c8eaf4a8a01394a0f000a3d85
Viacheslav Nosatskyi <nosatskyy.slava@gmail.com>
2020-08-11 21:51:09 +0300

    Adds cocktail quiz item. It contains random cocktail name and its ingredients

--- scripts/cocktailAPI.js
+++ scripts/cocktailAPI.js
@@ -1,17 +1,31 @@
-export class CocktailAPI {
+export class CocktailDBAPI {
   async getRandomCocktail() {
     let response = await fetch(
-      "https://www.thecocktaildb.com/api/json/v1/1/random.php",
-      {
-        method: "GET"
-      }
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     );
 
-    let json = null;
     if (response.ok) {
-        json = await response.json();
+      const cocktailData = await response.json();
+      return new Cocktail(cocktailData.drinks[0].strDrink, getIngredients(cocktailData.drinks[0]))
     }
-
-    return json;
   }
+}
+
+export class Cocktail {
+  constructor(name, ingredients = null) {
+    this.name = name;
+    this.ingredients = ingredients;
+  }
+}
+
+function getIngredients(drink) {
+  let variableName = "strIngredient";
+  let index = 1;
+  const ingredients = [];
+  while (drink[variableName + index]) {
+    ingredients[index - 1] = drink[variableName + index];
+    index++;
+  }
+
+  return ingredients;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-import {CocktailAPI} from "./cocktailAPI.js";
+import { CocktailDBAPI } from "./cocktailAPI.js";
+import { Cocktail } from "./cocktailAPI.js";
 
 //////////////////////////////
 // VARIABLES
@@ -9,16 +10,53 @@ const getRandomCocktailBtn = document.querySelector("#get-random-cocktail");
 // EVENTS
 //////////////////////////////
 getRandomCocktailBtn.addEventListener("click", (e) => {
-    let api = new CocktailAPI();
-    api.getRandomCocktail().then(result => console.log(result));
+  generateCocktailQuiz();
 });
 
 //////////////////////////////
 // HANDLERS
 //////////////////////////////
+async function generateCocktailQuiz() {
+  const cocktail = await getRandomCocktail();
+  cleanQuiz();
+  showQuiz(cocktail);
+}
 
 //////////////////////////////
 // HELPERS
 //////////////////////////////
+async function getRandomCocktail() {
+  let api = new CocktailDBAPI();
+  return await api.getRandomCocktail();
+}
 
+function showQuiz(cocktail) {
+    const cocktailQuiz = document.createElement("div");
+    cocktailQuiz.id = "cocktail-quiz";
+    cocktailQuiz.classList.add("cocktail-quiz");
+    document.querySelector("main").append(cocktailQuiz);
+    showCocktailName(cocktail.name);
+    showCocktailIngedients(cocktail.ingredients);
+}
 
+function cleanQuiz() {
+    const cocktailQuizItem = document.querySelector("#cocktail-quiz");
+    if (cocktailQuizItem) {
+        cocktailQuizItem.innerHTML = ""
+    }
+}
+
+function showCocktailName(name) {
+  const cocktailNameItem = document.createElement("h2");
+  cocktailNameItem.innerText = name;
+  document.querySelector("#cocktail-quiz").append(cocktailNameItem);
+}
+
+function showCocktailIngedients(ingredients) {
+  const cocktailIngredientsItem = document.createElement("ul");
+  for (const ingredient of ingredients) {
+    console.log(ingredient);
+    cocktailIngredientsItem.innerHTML += `<li>${ingredient}</li>`;
+  }
+  document.querySelector("#cocktail-quiz").append(cocktailIngredientsItem);
+}
