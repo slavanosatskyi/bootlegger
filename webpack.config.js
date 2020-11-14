c39e82e7e36a4f9fdba11f68afbafaa6669f89a8
Viacheslav Nosatskyi <nosatskyy.slava@gmail.com>
2020-11-14 23:39:15 +0200

    changes output directory for duild

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
     assetModuleFilename: "images/[name][ext]",
   },
