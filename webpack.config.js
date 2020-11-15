39ecb5b7bb1c394820f3520d7bb4de9adcc64b97
Viacheslav Nosatskyi <nosatskyy.slava@gmail.com>
2020-11-15 19:31:38 +0200

    Revert "Adds prod configuration for publicPath"
    This reverts commit d23c3c7eec43b9159a68ab96c2872605a2e72a7e.

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,46 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
-module.exports = (env) => {
-  return {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "docs"),
-      publicPath: env.NODE_ENV === "production" ? "bootleger/" : "/",
-      filename: "bundle.js",
-      assetModuleFilename: "images/[name][ext]",
-    },
-    devtool: "inline-source-map",
-    devServer: {
-      open: true,
-      port: 3000,
-      historyApiFallback: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                ["@babel/preset-env", { useBuiltIns: "usage" }],
-                ["@babel/preset-react"],
-              ],
-            },
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    publicPath: '/',
+    filename: "bundle.js",
+    assetModuleFilename: "images/[name][ext]",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { useBuiltIns: "usage" }],
+                      ["@babel/preset-react"]],
           },
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ template: "./index.html" }),
-      new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CleanWebpackPlugin(),
+  ],
 };
