d23c3c7eec43b9159a68ab96c2872605a2e72a7e
Viacheslav Nosatskyi <nosatskyy.slava@gmail.com>
2020-11-15 19:27:52 +0200

    Adds prod configuration for publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,50 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "docs"),
-    publicPath: '/',
-    filename: "bundle.js",
-    assetModuleFilename: "images/[name][ext]",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    open: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { useBuiltIns: "usage" }],
-                      ["@babel/preset-react"]],
+module.exports = (env) => {
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "docs"),
+      publicPath: env.NODE_ENV === "production" ? "bootleger/" : "/",
+      filename: "bundle.js",
+      assetModuleFilename: "images/[name][ext]",
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      open: true,
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["@babel/preset-env", { useBuiltIns: "usage" }],
+                ["@babel/preset-react"],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./index.html" }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./index.html" }),
-    new CleanWebpackPlugin(),
-  ],
+  };
 };
