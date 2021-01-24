b390440fd3c35ce10353ab4e8c9921555cebd9c3
Viacheslav Nosatskyi <nosatskyy.slava@gmail.com>
2021-01-24 19:36:23 +0200

    Adds animation to selection counter

--- src/components/quiz/SelectedIngredientsCounter/SelectedIngredientsCounter.js
+++ src/components/quiz/SelectedIngredientsCounter/SelectedIngredientsCounter.js
@@ -1,15 +1,47 @@
 import React from "react";
 
-const SelectedIngredientsCounter = ({
-  selectedIngredientsCount: selected,
-  ingredientsCount: total,
-  className
-}) => {
-  return (
-    <div className={className}>
-        {selected}/{total}
-    </div>
-  );
-};
+import "./SelectedIngredientsCounter.scss";
 
-export default SelectedIngredientsCounter;
+export default class SelectedIngredientsCounter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      animation: 0,
+    };
+
+    this.handleAnimationEnd = this.handleAnimationEnd.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.selectedIngredientsCount !== this.props.selectedIngredientsCount
+    ) {
+      this.setState({ animation: 1 });
+    }
+  }
+
+  handleAnimationEnd() {
+    this.setState({ animation: 0 });
+  }
+
+  render() {
+    const {
+      selectedIngredientsCount: selected,
+      ingredientsCount: total,
+      className,
+    } = this.props;
+
+    const { animation } = this.state;
+
+    return (
+      <div
+        className={`${className} counter`}
+        animation={animation}
+        onAnimationEnd={this.handleAnimationEnd}
+      >
+        {selected}/{total}
+      </div>
+    );
+  }
+}
